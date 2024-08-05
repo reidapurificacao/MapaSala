8b5b4f3bafb6d1ef24294dd946eee1224ec8f6c4
wagner <wagnersilvabittencourt@gmail.com>
2024-08-05 13:41:50 -0300

    Fizemos o DAO banco de dados

---
+++ MapaSala/DAO/ProfessorDAO.cs
@@ -0,0 +1,32 @@
+﻿using Model.Entitidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaSala.DAO
+{
+    class ProfessorDAO
+    {
+        private string LinhaConexao = "Server=LSO5MPF:Database=AULA_DS:User Id=sa;Password=admsasql;";
+        private SqlConnection Conexao;
+        public ProfessorDAO()
+        {
+            Conexao = new SqlConnection(LinhaConexao);
+        }
+            public void Inserir (ProfessoresEntidade professor)
+        {
+            Conexao.Open();
+            string query = "Insert into ´professores (Nome, Apelido) Values(@Nome, @Apelido)";
+            SqlCommand comando = new SqlCommand(query, Conexao);
+            SqlParameter parametro1 = new SqlParameter("@Nome", professor.Nome);
+            SqlParameter parametro2 = new SqlParameter("@Apelido", professor.Apelido);
+            comando.Parameters.Add(parametro1);
+            comando.Parameters.Add(parametro2);
+            comando.ExecuteNonQuery();
+            Conexao.Close();
+        }
+    }
+}
